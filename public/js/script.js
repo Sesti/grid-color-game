0a24fc45f6daadbfbb81efb61043bf5c72d5a19e
Sébastian Pilon <sebastian.pilon@gmail.com>
2019-12-15 22:39:13 -0500

    Added minimum grid width and height

--- public/js/script.js
+++ public/js/script.js
@@ -1,8 +1,14 @@
 import Gameboard from './Gameboard.js';
 import { selectInput } from './select.js';
 
+let defaultWidth = localStorage.getItem('gridWidth') || 10;
+let defaultHeight = localStorage.getItem('gridHeight') || 10;
+
 let inputWidth = document.getElementById('gridWidth');
 let inputHeight = document.getElementById('gridHeight');
+
+inputWidth.value = defaultWidth;
+inputHeight.value = defaultHeight;
 
 inputWidth.addEventListener('click', selectInput.bind(inputWidth));
 inputHeight.addEventListener('click', selectInput.bind(inputHeight));
@@ -15,8 +21,15 @@ inputWidth.addEventListener('change', updateGrid.bind(null, gameboard, "#grid", 
 inputHeight.addEventListener('change', updateGrid.bind(null, gameboard, "#grid", "#score"));
 
 function updateGrid(gameboard, selector, scoreSelector){
-    console.log(gameboard, selector, inputWidth.value, inputHeight.value);
+    if(inputWidth.value <= 3)
+        inputWidth.value = 4;
+
+    if(inputHeight.value <= 3)
+        inputHeight.value = 4;
     gameboard = new Gameboard(inputWidth.value, inputHeight.value);
     gameboard.attachTo(selector);
     gameboard.attachScore(scoreSelector);
+
+    localStorage.setItem('gridWidth', inputWidth.value);
+    localStorage.setItem('gridHeight', inputHeight.value);
 }
